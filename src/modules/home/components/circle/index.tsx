c4c8b0b1da1ae2b8cbd7f64df24259923fc93805
aesthetix-store <masrur.mmrs@gmail.com>
2024-05-23 21:05:07 +0600

    smoother hero section animation

--- src/modules/home/components/circle/index.tsx
+++ src/modules/home/components/circle/index.tsx
@@ -9,7 +9,7 @@ function Circle() {
     }, []);
 
   return (
-    <div className={clx("absolute h-[60vw] w-[60vw] bg-black rounded-full -z-10 lg:-bottom-[10%] lg:-right-[14%] md:-bottom-[40%] md:-right-[15%] sm:-bottom-[50%] sm:right-[0%] min-[300px]:-bottom-[5%] min-[300px]:right-[20%] transition duration-250 ease-in-out ", loaded?"scale-100":"scale-0")}>
+    <div className={clx("absolute h-[60vw] w-[60vw] bg-black rounded-full -z-10 lg:-bottom-[10%] lg:-right-[14%] md:-bottom-[40%] md:-right-[15%] sm:-bottom-[50%] sm:right-[0%] min-[300px]:-bottom-[5%] min-[300px]:right-[20%] transition duration-300 ease-in trasnform-gpu", loaded?"scale-100":"scale-0")}>
     </div>
   )
 }
